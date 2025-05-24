921e661b18cfdfb0604e96d37bc972f1a73e6b5d
Carlos Alvarez <carlos.alexander.dev@gmail.com>
2025-05-24 11:39:57 -0400

    feat(frontend): Added Nuxt as frontend with Quasar as UI.

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: ['nuxt-quasar-ui', '@nuxtjs/strapi'],
+  strapi: {
+    url: process.env.STRAPI_URL || 'http://localhost:1337',
+    // token: process.env.STRAPI_TOKEN || undefined,
+    prefix: '/api',
+    admin: '/admin',
+    version: 'v5',
+    cookie: {},
+    cookieName: 'strapi_jwt'
+  }
+})
