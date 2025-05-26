a96a896f08cae881d636c3fc4e616fd33a2ac4de
Carlos Alvarez <carlos.alexander.dev@gmail.com>
2025-05-25 22:54:56 -0400

    feat: Add mock page layout with hero and services sections

---
+++ frontend/layouts/mock.vue
@@ -0,0 +1,11 @@
+<template>
+    <q-layout view="lHh Lpr lFf">
+        <!-- Transparent Floating Header -->
+        <app-header />
+
+        <!-- Page Content -->
+        <q-page-container style="padding-top: 0;">
+            <slot />
+        </q-page-container>
+    </q-layout>
+</template>
