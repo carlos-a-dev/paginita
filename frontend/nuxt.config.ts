811a1cfdd707fb86d1c4cb2f5ba6d7d61ef88c9a
Carlos Alvarez <carlos.alexander.dev@gmail.com>
2025-05-28 23:54:19 -0400

    feat(frontend): Added eslint

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['nuxt-quasar-ui', '@nuxtjs/strapi'],
+  modules: ['nuxt-quasar-ui', '@nuxtjs/strapi', '@nuxt/eslint'],
   strapi: {
     url: process.env.NUXT_PUBLIC_STRAPI_URL || 'http://localhost:1337',
     devtools: false,
