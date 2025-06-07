8ae82d63e25d1b1ce80206e3b71b8f8674115584
Carlos Alvarez <carlos.alexander.dev@gmail.com>
2025-06-07 12:27:38 -0400

    feat: update email configuration to use SendGrid provider and add necessary environment variables

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,3 +1,16 @@
-export default () => ({
-    'material-symbols': true
+export default ({ env }) => ({
+    'material-symbols': true,
+    email: {
+    config: {
+      provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: env('SENDGRID_EMAIL_FROM'),
+        defaultReplyTo: env('SENDGRID_EMAIL_REPLY_TO'),
+        testAddress: env('SENDGRID_EMAIL_TEST_ADDRESS'),
+      },
+    },
+  },
 });
