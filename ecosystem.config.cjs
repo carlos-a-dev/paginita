5b2b0ff9f5d7d4932a84359eafb2b4ed467a396a
Carlos Alvarez <carlos.alexander.dev@gmail.com>
2025-06-07 10:14:02 -0400

    feat: add not-pnpm-workspace.yaml for built dependencies and update ecosystem.config.cjs for frontend and backend app names

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,6 +3,7 @@ require('dotenv').config()
 module.exports = {
   apps: [
     {
+      name: 'paginita-frontend',
       script: '.output/server/index.mjs',
       max_memory_restart: '200M',
 
@@ -22,21 +23,28 @@ module.exports = {
         exec_mode: 'cluster',
         instances: 0,
       },
-    //   env_stage: {
-    //     name: 'paginita-stage',
-    //     cwd: '/var/www/stage.alvasori.net/web/',
-    //     NODE_ENV: 'production',
-    //     PORT: 5001,
-    //     mode: 'fork',
-    //     instances: '1',
-    //   },
-    //   env_development: {
-    //     name: 'alvasori-dev',
-    //     NODE_ENV: 'development',
-    //     PORT: 3000,
-    //     exec_mode: 'fork',
-    //     instances: '1',
-    //   },
+    },
+    {
+      name: 'paginita-backend',
+      script: 'pnpm',
+      args: 'start',
+      max_memory_restart: '200M',
+
+      // Logging
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+      merge_logs: true,
+      log_date_format: 'DD-MM HH:mm:ss Z',
+      log_type: 'json',
+
+      // Env Specific Config
+      env_production: {
+        name: 'paginita',
+        cwd: '/var/www/paginita/backend/',
+        NODE_ENV: 'production',
+        exec_mode: 'fork',
+        instances: 0,
+      },
     },
   ],
 }
