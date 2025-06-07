c0a60e4518aebfb141050c61f57cb6ac99239fe1
Carlos Alvarez <carlos.alexander.dev@gmail.com>
2025-06-07 10:37:28 -0400

    feat: add .gitignore to exclude log files and update ecosystem configuration for frontend and backend applications

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,49 +1,55 @@
-// require('dotenv').config()
-
 module.exports = {
   apps: [
     {
       name: 'paginita-frontend',
-      script: '.output/server/index.mjs',
+      script: 'node',
+      args: '.output/server/index.mjs',
+      cwd: '/var/www/paginita/frontend/',
       max_memory_restart: '200M',
 
       // Logging
-      out_file: './logs/out.log',
-      error_file: './logs/error.log',
+      out_file: './logs/frontend_out.log',
+      error_file: './logs/frontend_error.log',
       merge_logs: true,
       log_date_format: 'DD-MM HH:mm:ss Z',
       log_type: 'json',
 
-      // Env Specific Config
+      exec_mode: 'cluster',
+      instances: 0, // 0 = max CPU cores
+
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3000,
+      },
+
       env_production: {
-        name: 'paginita',
-        cwd: '/var/www/paginita/frontend/',
         NODE_ENV: 'production',
         PORT: 5000,
-        exec_mode: 'cluster',
-        instances: 0,
       },
     },
     {
       name: 'paginita-backend',
       script: 'pnpm',
       args: 'start',
+      cwd: '/var/www/paginita/backend/',
       max_memory_restart: '200M',
 
       // Logging
-      out_file: './logs/out.log',
-      error_file: './logs/error.log',
+      out_file: './logs/backend_out.log',
+      error_file: './logs/backend_error.log',
       merge_logs: true,
       log_date_format: 'DD-MM HH:mm:ss Z',
       log_type: 'json',
 
-      // Env Specific Config
+      exec_mode: 'fork',
+      instances: 1,
+
+      env: {
+        NODE_ENV: 'development',
+      },
+
       env_production: {
-        name: 'paginita',
-        cwd: '/var/www/paginita/backend/',
         NODE_ENV: 'production',
-        exec_mode: 'fork',
-        instances: 0,
       },
     },
   ],
