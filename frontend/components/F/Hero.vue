883861579e1d1d4e89c9f067825247abb0681179
Carlos Alvarez <carlos.alexander.dev@gmail.com>
2025-06-10 02:12:42 -0400

    feat: refactor Hero component to enhance image handling and remove Hero2 component

--- frontend/components/F/Hero.vue
+++ frontend/components/F/Hero.vue
@@ -1,11 +1,41 @@
 <template>
-  <q-card>
+  <q-card style="border-radius: 2em;">
     <q-img
       v-if="props.data.background && imgProps.src"
       v-bind="imgProps"
       fetchpriority="high"
       loading="eager"
-    />
+      class="hero-img"
+      fit="cover"
+      :ratio="$q.screen.width / $q.screen.height"
+      :height="$q.screen.height - 80 + 'px'"
+    >
+      <div class="fit flex flex-center text-center transparent">
+        <div :class="[$q.screen.gt.xs ? 'q-pa-xl' : 'q-pa-none']">
+          <h1
+            v-if="data.title"
+            class="text-h2 text-bold"
+          >
+            {{ data.title }}
+          </h1>
+          <p
+            v-if="data.message"
+            class="text-h4 q-mt-md"
+          >
+            {{ data.message }}
+          </p>
+          <q-btn
+            v-if="data.callToAction && data.link"
+            unelevated
+            color="secondary"
+            :label="data.callToAction"
+            class="q-mt-lg"
+            :to="data.link"
+            size="lg"
+          />
+        </div>
+      </div>
+    </q-img>
   </q-card>
 </template>
 
